3ce99d307ff773a6e165196c74df9c89025a1013
Lyn Scott <lennord@gmail.com>
2017-12-23 18:17:41 -0800

    Fixed node env stuff from lsphysique to client

--- server.js
+++ server.js
@@ -311,10 +311,10 @@ app.post('/api/stripe', async (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, './lsphysique/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './lsphysique/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
   });
 }
 
