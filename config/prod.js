ca434d2e5ba080f6b7ad4c2c56b00d0f00dc008d
Lyn Scott <lennord@gmail.com>
2017-12-24 10:01:16 -0800

    Added dev facebook keys

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,8 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   stripeSecret: process.env.STRIPE_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
