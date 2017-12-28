0de0b345003aaf4df2a996c65ecc54129a5e7087
Lyn Scott <lennord@gmail.com>
2017-12-27 19:39:19 -0800

    Updated show plan with list view

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   //production
   module.exports = require('./prod');
-} else {
+} else if (process.env.NODE_ENV != 'production') {
   //development
-  module.exports = require('./dev');
+  module.exports = require('./dev.js');
 }
