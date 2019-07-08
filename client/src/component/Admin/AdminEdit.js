56bcba31354c16f353a5105f59ed9ebbbef1217b
lynscott <lennord@gmail.com>
2019-07-07 17:12:08 -0700

    Updates for admin panel

---
+++ client/src/component/Admin/AdminEdit.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import {
+  Edit,
+  SimpleForm,
+  TextInput,
+  SimpleFormIterator,
+  DateInput,
+  ArrayInput,
+  NumberInput,
+  BooleanInput,
+  DisabledInput,
+  ReferenceInput,
+  ChipField,
+  SingleFieldList,
+  EditButton
+} from 'react-admin'
+
+export const ProfileEdit = props => (
+  <Edit {...props}>
+    <SimpleForm>
+        <DisabledInput source="id" />
+      <TextInput source="id" />
+      <TextInput source="affirmation" />
+      <TextInput source="messages" />
+      <TextInput source="events" />
+      <TextInput source="progressPics" />
+      <ArrayInput source="weighIns">
+        <SimpleFormIterator>
+          <DateInput source="weight" />
+          <DateInput source="date" />
+        </SimpleFormIterator>
+      </ArrayInput>
+      <NumberInput source="macros.fat" />
+      <TextInput source="currentGoal.text" />
+      <ArrayInput source="nutritionSchedule">
+        <SimpleFormIterator>
+          <ArrayInput source="items">
+            <SimpleFormIterator>
+              <TextInput source="name" />
+              <TextInput source="serving_label" />
+              <NumberInput source="serving" />
+              <DateInput source="baseCal" />
+              <TextInput source="baseFats" />
+              <TextInput source="baseCarb" />
+              <TextInput source="baseProtein" />
+              <TextInput source="calories" />
+              <TextInput source="fats" />
+              <TextInput source="carb" />
+              <TextInput source="protein" />
+              <BooleanInput source="active" />
+              <TextInput source="id" />
+            </SimpleFormIterator>
+          </ArrayInput>
+          <TextInput source="time" />
+        </SimpleFormIterator>
+      </ArrayInput>
+      <ArrayInput source="nutritionItems">
+        <SimpleFormIterator>
+          <TextInput source="name" />
+          <TextInput source="serving_label" />
+          <DateInput source="serving" />
+          <DateInput source="baseCal" />
+          <TextInput source="baseFats" />
+          <TextInput source="baseCarb" />
+          <TextInput source="baseProtein" />
+          <TextInput source="calories" />
+          <TextInput source="fats" />
+          <TextInput source="carb" />
+          <TextInput source="protein" />
+          <BooleanInput source="active" />
+          <TextInput source="id" />
+        </SimpleFormIterator>
+      </ArrayInput>
+      <NumberInput source="nutritionCalories" />
+      {/* <NumberInput source="__v" /> */}
+      <NumberInput source="baseSomaType.macro.fat" />
+      <BooleanInput source="isAdmin" />
+      <DateInput source="calories" />
+      {/* <TextInput source="_user" /> */}
+      <TextInput source="email" />
+      {/* <ReferenceInput source="_id" reference="s">
+        <SelectInput optionText="id" />
+      </ReferenceInput> */}
+    </SimpleForm>
+  </Edit>
+)
--- client/src/component/Admin/AdminFiles.js
+++ client/src/component/Admin/AdminFiles.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { List, Datagrid, TextField, EmailField, NumberField, ReferenceField, ArrayField, DateField, BooleanField, ChipField, 
-    SingleFieldList  } from 'react-admin'
+    SingleFieldList, EditButton  } from 'react-admin'
+
+// DOC For React Admin - https://marmelab.com/react-admin/Tutorial.html#setting-up
 
 export const ExerciseList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
             <TextField source="name" />
-            <TextField source="id" />
+            {/* <TextField source="id" /> */}
             {/* <TextField source="DBid" /> */}
         </Datagrid>
     </List>
@@ -15,8 +17,8 @@ export const ExerciseList = props => (
 
 export const ProfileList = props => (
     <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" />
+        <Datagrid >
+            {/* <TextField source="id" /> */}
             <TextField source="affirmation" />
             <TextField source="messages" />
             <TextField source="events" />
@@ -25,15 +27,51 @@ export const ProfileList = props => (
             <TextField source="macros" />
             <NumberField source="currentGoal.text" />
             {/* <TextField source="nutritionSchedule" /> */}
-            {/* <ArrayField source="nutritionItems"><SingleFieldList><ChipField source="carb" /></SingleFieldList></ArrayField> */}
-            {/* <NumberField source="__v" /> */}
+            <ArrayField source="nutritionItems"><SingleFieldList><ChipField source="carb" /></SingleFieldList></ArrayField>
             <NumberField source="baseSomaType.type" />
             <NumberField source="calories" />
-            {/* <TextField source="_user" /> */}
             <EmailField source="email" />
-            {/* <ReferenceField source="_id" reference="s"><TextField source="_id" /></ReferenceField> */}
+            <ReferenceField source="_user" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
             <NumberField source="nutritionCalories" />
             <BooleanField source="isAdmin" />
+            <EditButton />
+        </Datagrid>
+    </List>
+)
+
+
+export const UserList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            {/* <TextField source="id" /> */}
+            {/* <TextField source="plans" /> */}
+            {/* <NumberField source="__v" /> */}
+            {/* <TextField source="img" /> */}
+            <TextField source="gender" />
+            <TextField source="name" />
+            <TextField source="googleID" />
+            <TextField source="provider" />
+            <EmailField source="email" />
+            <TextField source="facebookID" />
+            <NumberField source="currentWeight" />
+            <NumberField source="height" />
+            <DateField source="startDate" />
+        </Datagrid>
+    </List>
+)
+
+
+
+export const WorkoutList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            {/* <TextField source="id" /> */}
+            <TextField source="title" />
+            <TextField source="type" />
+            <ArrayField source="exercises"><SingleFieldList><ChipField source="note" /></SingleFieldList></ArrayField>
+            {/* <ReferenceField source="_id" reference="s"><TextField source="id" /></ReferenceField> */}
         </Datagrid>
     </List>
 )
